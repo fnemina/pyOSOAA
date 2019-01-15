961716eba9c9349f8319c29d6eec0f9f4c440cb5
Francisco Nemiña <fnemina@gmail.com>
2019-01-15 12:05:43 -0300

    Add SEA class.

--- pyOSOAA/osoaa.py
+++ pyOSOAA/osoaa.py
@@ -3,7 +3,7 @@ import os
 
 
 class LND(object):
-    """This is a lognormal distribution for the different models."""
+    """ This is a lognormal distribution for the different models. """
 
     def __init__(self, mrwa, miwa, sdradius, sdvar, rate):
         """ Init function for the lognormal distribution.
@@ -24,7 +24,7 @@ class LND(object):
 
 
 class JD(object):
-    """This is a Junge distribution for the different models."""
+    """ This is a Junge distribution for the different models. """
 
     def __init__(self, mrwa, miwa, slope, rmin, rmax, rate):
         """ Init function for the lognormal distribution.
@@ -44,3 +44,29 @@ class JD(object):
         self.rmin = rmin
         self.rmax = rmax
         self.rate = rate
+
+
+class SEA(object):
+    """ This is the SEA class which defines the interfaces at the bottom of the
+        ocean and the interface with the air. """
+
+    def __init__(self, surfalb, bottype, botalb, log, wind, dir, ind, depth):
+        """ Init function for the SEA class.
+            surfalb     Foam lambertian reflectance for the current wavelength.
+            bottype     Type of seabottom for albedo definition.
+            botalb      Seabottom albedo for botype = 1.
+            log         Log filename for surface computations
+            wind        Wind speed at seasurface (m/s)
+            dir         Directory for surface files.
+            ind         Surface/atmosphere refraction index, air = 1.
+            depth       Sea depth in meters.
+            """
+
+        self.surfalb = surfalb
+        self.bottype = bottype
+        self.botalb = botalb
+        self.log = log
+        self.wind = wind
+        self.dir = dir
+        self.ind = ind
+        self.depth = depth
