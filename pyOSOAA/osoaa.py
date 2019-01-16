291802c4bdf075b01e4579bbe439b55f6f1a5918
Francisco Nemiña <fnemina@gmail.com>
2019-01-16 10:15:43 -0300

    We add the PYTHO class to the project.

--- pyOSOAA/osoaa.py
+++ pyOSOAA/osoaa.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 
+
 class LND(object):
     """ This is a lognormal distribution for the different models. """
 
@@ -26,7 +27,7 @@ class JD(object):
     """ This is a Junge distribution for the different models. """
 
     def __init__(self, mrwa, miwa, slope, rmin, rmax, rate):
-        """ Init function for the lognormal distribution.
+        """ Init function for the Junges distribution.
             mrwa        Real part of the refractive index at simulation
                         wavelength
             miwa        Imaginary part of the refractive index at simulation
@@ -132,3 +133,115 @@ class DIRMIE(object):
     """
     hid = None
     aer = None
+
+
+class GP(object):
+    """ Gaussian profile class definition."""
+
+    def __init__(self, chlbg, deep, width):
+        """ Init function for the gaussian profiles
+        chlbg       Constant background biomass (mg/m3)
+        deep        Maximun deep of the gaussian profile (m)
+        width       Peak width of the gaussian profile (m)
+        """
+
+        self.chlbg = chlbg
+        self.deep = deep
+        self.width = width
+
+
+class PHYTO(object):
+    """ Phytoplanckton class"""
+    # Profile types
+    Homogeneous = 1
+    Gaussian = 2
+    UserDefined = 3
+
+    def __init__(self, chl):
+        """ Init function for the Phytoplanckton profiles
+        chl     Phytoplanckton concentration
+        """
+
+        self.chl = chl
+        self.profiltype = self.Homogeneous
+        self.jd = None
+        self.sm = None
+        self.tm = None
+
+    def SetPrimaryMode(self, mrwa, miwa, slope, rmin, rmax, rate):
+        """ Sets the primary mode using Junge's law
+            mrwa        Real part of the refractive index at simulation
+                        wavelength
+            miwa        Imaginary part of the refractive index at simulation
+                        wavelength
+            slope       Slope of Junge's law
+            rmin        Minimal radius for Junge's law
+            rmax        Maximal radius for Junge's law
+            rate        Ratio of the mode in the global distribution
+            """
+
+        self.jd = JD(mrwa, miwa, slope, rmin, rmax, rate)
+
+    def SetSecondaryMode(self, mrwa, miwa, sdradius, sdvar, rate):
+        """ Sets the secondary mode using lognormal distribution
+            mrwa        Real part of the refractive index at simulation
+                        wavelength
+            miwa        Imaginary part of the refractive index at simulation
+                        wavelength
+            sdradius    Modal radius of the lognormal distribution
+            sdvar       Standar deviation of the lognormal distribution
+            rate        Ratio of the mode in the global distribution
+            """
+
+        self.sm = LND(mrwa, miwa, sdradius, sdvar, rate)
+
+    def SetTertiaryMode(self, mrwa, miwa, sdradius, sdvar, rate):
+        """ Sets the secondary mode using lognormal distribution
+            mrwa        Real part of the refractive index at simulation
+                        wavelength
+            miwa        Imaginary part of the refractive index at simulation
+                        wavelength
+            sdradius    Modal radius of the lognormal distribution
+            sdvar       Standar deviation of the lognormal distribution
+            rate        Ratio of the mode in the global distribution
+            """
+
+        self.tm = LND(mrwa, miwa, sdradius, sdvar, rate)
+
+    def SetProfilType(self, profiltype, chlbg, deep, width, userfile):
+        """ This method sets the profile type for the Phytoplanckton
+            distribution. This also configures the parameters for each
+            profile type
+
+            profiltype      Profile for the chlorophyl distribution.
+                            1 - homogeneous profile
+                            2 - Gaussian profile
+                            3 - User defined profile
+
+            for the Gaussian profiltype
+            chlbg           Constant background biomass (mg/m3)
+            deep            Maximun deep of the gaussian profile (m)
+            width           Peak width of the gaussian profile (m)
+
+            for the User defined profile
+            userfile        Userfile describing the chlorophyll profile
+            """
+
+        if profiltype not in [self.Homogeneous,
+                              self.Gaussian,
+                              self.UserDefined]:
+            raise Exception("Invalid profile type.")
+
+        self.profiltype = profiltype
+
+        if profiltype == self.Gaussian:
+            # We confifigure the gaussian profile
+            self.gp = GP(chlbg, deep, width)
+
+        elif profiltype == self.UserDefined:
+            # We configure the user defined profile
+            try:
+                open(userfile, 'r')
+                self.usefile = userfile
+            except FileNotFoundError:
+                print("File {} not found".format(userfile))
