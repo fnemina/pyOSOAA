86e70e46062e6c1572fe4f9a407fc492f330f921
Francisco Nemiña <fnemina@gmail.com>
2019-01-25 13:37:38 -0300

    Update setup.py version.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     name                  = "pyOSOAA",
     packages              = ['pyOSOAA'],
     install_requires      = REQS,
-    version               = "0.3 alpha",
+    version               = "0.3",
     author                = "Francisco Nemiña",
     author_email          = "fnemina@conae.gov.ar",
     description           = SHORT_DESCRIPTION,
