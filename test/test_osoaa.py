3cc57da668111fca4a7902a94534bbd347a11698
Francisco Nemiña <fnemina@gmail.com>
2019-01-29 12:25:05 -0300

    We add test for DIERMIE, GP, PHYTO, SED, YS and DET. We include a test file.

--- test/test_osoaa.py
+++ test/test_osoaa.py
@@ -36,6 +36,136 @@ class TestOSOAAClasses(unittest.TestCase):
         self.assertEqual(pyOSOAA.RESULTS().advdown, "resfile_advdown.txt")
         self.assertEqual(pyOSOAA.RESULTS().vsz, "resfile_vsz.txt")
 
+    def testDIRMIE(self):
+        self.assertEqual(pyOSOAA.DIRMIE("").hyd, "/DATABASE/MIE_HYD")
+        self.assertEqual(pyOSOAA.DIRMIE("").aer, "/DATABASE/MIE_AER")
+        self.assertEqual(pyOSOAA.DIRMIE("").sea, "/DATABASE/SURF_MATR")
+
+    def testGP(self):
+        self.assertEqual(pyOSOAA.GP(1, 5, 2).chlbg, 1)
+        self.assertEqual(pyOSOAA.GP(1, 5, 2).deep, 5)
+        self.assertEqual(pyOSOAA.GP(1, 5, 2).width, 2)
+
+    def testPHYTO(self):
+        phyto = pyOSOAA.PHYTO()
+
+        self.assertEqual(phyto.chl, 0.2)
+        self.assertEqual(phyto.profiltype, phyto.Homogeneous)
+        self.assertEqual(phyto.jd.mrwa, 1.05)
+        self.assertEqual(phyto.jd.miwa, -0.0)
+        self.assertEqual(phyto.jd.slope, 4.0)
+        self.assertEqual(phyto.jd.rmin, 0.01)
+        self.assertEqual(phyto.jd.rmax, 200)
+        self.assertEqual(phyto.jd.rate, 1.0)
+        self.assertIsNone(phyto.sm)
+        self.assertIsNone(phyto.tm)
+
+        # Test the method to set the primary mode
+        phyto.SetPrimaryMode(1.06, 0.01, 4.2, 0.02, 210, 0.8)
+        self.assertEqual(phyto.jd.mrwa, 1.06)
+        self.assertEqual(phyto.jd.miwa, 0.01)
+        self.assertEqual(phyto.jd.slope, 4.2)
+        self.assertEqual(phyto.jd.rmin, 0.02)
+        self.assertEqual(phyto.jd.rmax, 210)
+        self.assertEqual(phyto.jd.rate, 0.8)
+        self.assertIsNone(phyto.sm)
+        self.assertIsNone(phyto.tm)
+
+        # Test the method to set the secundary mode
+        phyto.SetSecondaryMode(1.05, -0.0, 1, 0.2, 0.1)
+        self.assertEqual(phyto.sm.mrwa, 1.05)
+        self.assertEqual(phyto.sm.miwa, -0.0)
+        self.assertEqual(phyto.sm.sdradius, 1)
+        self.assertEqual(phyto.sm.sdvar, 0.2)
+        self.assertEqual(phyto.sm.rate, 0.1)
+        self.assertIsNone(phyto.tm)
+
+        # Test the method to set the tertiary mode
+        phyto.SetTertiaryMode(1.05, -0.0, 1, 0.2, 0.1)
+        self.assertEqual(phyto.tm.mrwa, 1.05)
+        self.assertEqual(phyto.tm.miwa, -0.0)
+        self.assertEqual(phyto.tm.sdradius, 1)
+        self.assertEqual(phyto.tm.sdvar, 0.2)
+        self.assertEqual(phyto.tm.rate, 0.1)
+
+        # Check profile type
+
+        phyto.SetProfilType(phyto.Homogeneous)
+        self.assertEqual(phyto.profiltype, phyto.Homogeneous)
+
+        phyto.SetProfilType(phyto.Gaussian, chlbg=0.1, deep=1, width=2)
+        self.assertEqual(phyto.profiltype, phyto.Gaussian)
+        self.assertEqual(phyto.gp.chlbg, 0.1)
+        self.assertEqual(phyto.gp.deep, 1)
+        self.assertEqual(phyto.gp.width, 2)
+
+        phyto.SetProfilType(phyto.UserDefined, userfile="test/profile.txt")
+        self.assertEqual(phyto.usefile, "test/profile.txt")
+
+        phyto.SetProfilType(phyto.UserDefined, userfile="fail")
+        self.assertRaises(FileNotFoundError)
+
+        # We check the fail check
+        with self.assertRaises(Exception) as context:
+            phyto.SetProfilType("Bad")
+        self.assertTrue("Invalid profile type." in str(context.exception))
+
+    def testSED(self):
+        sed = pyOSOAA.SED()
+
+        self.assertEqual(sed.csed, 0.0)
+        self.assertIsNone(sed.jd)
+        self.assertIsNone(sed.sm)
+        self.assertIsNone(sed.tm)
+
+        # Test the method to set the primary mode
+        sed.SetPrimaryMode(1.06, 0.01, 4.2, 0.02, 210, 0.8)
+        self.assertEqual(sed.jd.mrwa, 1.06)
+        self.assertEqual(sed.jd.miwa, 0.01)
+        self.assertEqual(sed.jd.slope, 4.2)
+        self.assertEqual(sed.jd.rmin, 0.02)
+        self.assertEqual(sed.jd.rmax, 210)
+        self.assertEqual(sed.jd.rate, 0.8)
+        self.assertIsNone(sed.sm)
+        self.assertIsNone(sed.tm)
+
+        # Test the method to set the secundary mode
+        sed.SetSecondaryMode(1.05, -0.0, 1, 0.2, 0.1)
+        self.assertEqual(sed.sm.mrwa, 1.05)
+        self.assertEqual(sed.sm.miwa, -0.0)
+        self.assertEqual(sed.sm.sdradius, 1)
+        self.assertEqual(sed.sm.sdvar, 0.2)
+        self.assertEqual(sed.sm.rate, 0.1)
+        self.assertIsNone(sed.tm)
+
+        # Test the method to set the tertiary mode
+        sed.SetTertiaryMode(1.05, -0.0, 1, 0.2, 0.1)
+        self.assertEqual(sed.tm.mrwa, 1.05)
+        self.assertEqual(sed.tm.miwa, -0.0)
+        self.assertEqual(sed.tm.sdradius, 1)
+        self.assertEqual(sed.tm.sdvar, 0.2)
+        self.assertEqual(sed.tm.rate, 0.1)
+
+    def testYS(self):
+        self.assertEqual(pyOSOAA.YS().abs440, 0.0)
+        self.assertIsNone(pyOSOAA.YS().swa)
+        self.assertEqual(pyOSOAA.YS(1.0, 1.0).abs440, 1.0)
+        self.assertEqual(pyOSOAA.YS(1.0, 2.0).swa, 2.0)
+        # We check the fail check
+        with self.assertRaises(Exception) as context:
+            pyOSOAA.YS(-1)
+        self.assertTrue("Invalid absorption value." in str(context.exception))
+
+    def testDET(self):
+        self.assertEqual(pyOSOAA.DET().abs440, 0.0)
+        self.assertIsNone(pyOSOAA.DET().swa)
+        self.assertEqual(pyOSOAA.DET(1.0, 1.0).abs440, 1.0)
+        self.assertEqual(pyOSOAA.DET(1.0, 2.0).swa, 2.0)
+        # We check the fail check
+        with self.assertRaises(Exception) as context:
+            pyOSOAA.DET(-1)
+        self.assertTrue("Invalid absorption value." in str(context.exception))
+
 
 if __name__ == '__main__':
     unittest.main()
