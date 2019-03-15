208170223373842ddcb8f4534f818e769e75acad
Francisco Nemiña <fnemina@conae.gov.ar>
2019-03-15 10:20:34 -0300

    Add OSOAA helper to run the model for different angles.

--- pyOSOAA/osoaahelpers.py
+++ pyOSOAA/osoaahelpers.py
@@ -53,3 +53,65 @@ def RunWavelengths(s, wavelengths=[0.550], angle=0, output="I", tau=False):
         return values, s.outputs.profileatm.tau[-1]
 
     return values
+
+
+def RunAngles(s, wavelength=0.550, thetav=0, thetas=40, phi=90,
+              output="I"):
+    """ This method run the simulation for a given pyOSOAA object for a set of
+        angles and a wavelength and returns the output from the file vsVZA
+
+        Parameters
+        ----------
+        s           The pyOSOAA object for which we want to run the simulation.
+        wavelengths An interable with the wavelengths in micrometers for which
+                    to run the simulations
+        thetav      The view angle in degrees for which to run the simulation.
+        thetas      The sun angle in degrees for which to run the simulation.
+        phi         The relative acimuth angle in degrees for which to run the
+                    simulation.
+        output      The name of the output we want to compute as an string
+                    I           Stokes parameter at output level Z (in sr-1)
+                                normalised to the extraterrestrial solar
+                                irradiance
+                                (PI * L(z) / Esun)
+                    refl        Reflectance at output level Z
+                    (PI * L(z) / Ed(z))
+                    polrate     Degree of polarization (%)
+                    lpol        Polarized intensity at output level Z (in sr-1)
+                                normalised to the extraterrestrial solar
+                                irradiance
+                                (PI * Lpol(z) / Esun)
+                    reflpol     Polarized reflectance at output level Z
+                                (PI * Lpol(z) / Ed(z))
+        """
+
+    if output not in ["I", "refl", "polrate", "lpol", "reflpol"]:
+        raise(ValueError("Wrong output variable."))
+
+    # We check the type for the angles
+    if type(thetav) is int or type(thetav) is float:
+        thetav = np.array([thetav])
+    if type(thetas) is int or type(thetas) is float:
+        thetas = np.array([thetas])
+    if type(phi) is int or type(phi) is float:
+        phi = np.array([phi])
+
+    values = -np.ones((thetav.size, thetas.size, phi.size))
+
+    # We set the wavelength and run the simulation
+    s.wa = wavelength
+
+    for idxthetas, valthetas in np.ndenumerate(thetas):
+        for idxphi, valphi in np.ndenumerate(phi):
+            s.view.phi = valphi
+            s.ang.thetas = valthetas
+            s.run()
+            # Convert the output to a directory
+            results = vars(s.outputs.vsvza)
+            # We interpolte the values and add it to a numpy array
+            f = interp1d(results['vza'], results[output])
+            # We reshape the array to make the asignement
+            tempval = f(thetav).reshape(thetav.size, 1)
+            values[:, idxthetas, idxphi] = tempval[:]
+
+    return values
